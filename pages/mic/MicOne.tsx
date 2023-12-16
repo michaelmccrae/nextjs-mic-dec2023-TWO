7a61b33d600e913ad6bd8752c2c5897e674dfd7a
michaelmccrae <mike@mccrae.ca>
2023-12-16 10:00:15 -0800

    first commit

---
+++ pages/mic/MicOne.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { AudioRecorder } from 'react-audio-voice-recorder';
+
+const MicOne = () => {
+
+    const addAudioElement = (blob: Blob) => {
+        const url = URL.createObjectURL(blob);
+        const audio = document.createElement("audio");
+        audio.src = url;
+        audio.controls = true;
+        document.body.appendChild(audio);
+      };
+
+  return (
+    <div>
+        <AudioRecorder 
+      onRecordingComplete={addAudioElement}
+      audioTrackConstraints={{
+        noiseSuppression: true,
+        echoCancellation: true,
+      }} 
+      downloadOnSavePress={true}
+      downloadFileExtension="wav"
+    />
+      
+    </div>
+  )
+}
+
+export default MicOne;
---
+++ pages/mic/index.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import MicOne from './MicOne'
+
+export default function Home() {
+  return (
+   
+    <main>
+      <h1>Working</h1>
+      <MicOne />
+      
+    </main>
+  )
+}
